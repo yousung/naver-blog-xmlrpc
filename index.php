dd5e1fd2b4c257a7325de1b16bd01d06e08f600a
안유성 <nug22kr@gmail.com>
2018-11-21 09:45:13 +0900

    fixed git ignore

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-use Lovizu\NaverXmlRpc\NaverBlogXml;
-
-$post = new NaverBlogXml('nug22', '86d94d0786130ababdeb3aef8fca4864');
-var_dump($post->delBlog('221298547224'));
